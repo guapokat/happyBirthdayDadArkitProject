6f1722d9da25585e80f218f82ce6759f26f3f968
guapokat <virgilmartinez@gmail.com>
2018-03-09 11:34:04 -0600

    Added 3D sign post with wood material mapped to it. Still have to add png images and message. Maybe add confetti effect and music!

--- happyBirthdayDad/ViewController.swift
+++ happyBirthdayDad/ViewController.swift
@@ -16,65 +16,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // Root node
+        let node = SCNNode()
+        let boxNode = SCNNode()
+        // Cylinder
+        let cylinder = SCNCylinder(radius: 0.01, height: 0.8)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/wood.jpg")
+        cylinder.materials = [material]
+        node.position = SCNVector3(0,-0.6,-0.8)
+        node.geometry = cylinder
+        sceneView.scene.rootNode.addChildNode(node)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let sign = SCNBox(width: 0.35, height: 0.2, length: 0.02, chamferRadius: 0)
+        sign.materials = [material]
+        node.addChildNode(boxNode)
+        boxNode.geometry = sign
+        boxNode.position = SCNVector3(0,0.33,0.02)
+        // Lighting
+        sceneView.autoenablesDefaultLighting = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        // Debug stuffs
+        //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
         // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
